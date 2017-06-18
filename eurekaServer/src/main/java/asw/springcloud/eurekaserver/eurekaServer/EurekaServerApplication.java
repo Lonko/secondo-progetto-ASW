42abf1bc0e335749291134057aef44bc9af5d9e9
Marco Monteleone <mar.monteleone@gmail.com>
2017-06-18 12:09:29 +0200

    added new eureka server

---
+++ eurekaServer/src/main/java/asw/springcloud/eurekaserver/eurekaServer/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package asw.springcloud.eurekaserver.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
